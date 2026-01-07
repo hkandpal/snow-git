6ceca2a436defe7e880dbecdea1ea855cc3ccb2f
hkandpal <hk69@nyu.edu>
2026-01-07 21:25:55 +0000

    t

--- Email_Attachement.sql
+++ Email_Attachement.sql
@@ -1,13 +1,13 @@
-CREATE NOTIFICATION INTEGRATION EMAIL_INT
+CREATE or replace NOTIFICATION INTEGRATION EMAIL_INT
   TYPE = EMAIL
   ENABLED = TRUE
   ALLOWED_RECIPIENTS = ( 'hk69@nyu.edu' )
   DEFAULT_SUBJECT = 'Email Notification';
 
-CREATE DATABASE FILES_DB;
+CREATE or replace DATABASE FILES_DB;
 USE DATABASE FILES_DB;
 CREATE SCHEMA CSV_FILES;
-CREATE STAGE PUBLIC_FILES ENCRYPTION = (TYPE = 'SNOWFLAKE_SSE');
+CREATE or replace STAGE PUBLIC_FILES ENCRYPTION = (TYPE = 'SNOWFLAKE_SSE');
 
 
 
@@ -16,3 +16,12 @@ create schema TEST_SCHEMA;
 
 create or replace view TEST.TEST_SCHEMA.EMP as select * from data_db.sch.customers; 
 
+
+select $1 , $2 from @FILES_DB.CSV_FILES.PUBLIC_FILES;
+select GET_PRESIGNED_URL(@FILES_DB.CSV_FILES.PUBLIC_FILES,'employee_details_geit0k_f.csv');
+select GET_PRESIGNED_URL(@FILES_DB.CSV_FILES.PUBLIC_FILES, 'employee_details_geit0k_f.csv',8600) as signed_url;
+select GET_PRESIGNED_URL(@FILES_DB.CSV_FILES.PUBLIC_FILES, 'employee_details_lug4g0j0.csv',8600) as signed_url;
+SELECT GET_STAGE_LOCATION(@FILES_DB.CSV_FILES.PUBLIC_FILES);
+ 
+select GET_PRESIGNED_URL(@FILES_DB.CSV_FILES.PUBLIC_FILES, 'employee_details_zqcgpv29.csv',8600) as signed_url;
+employee_details_geit0k_f.csv
--- Snowflake_Intelligence.sql
+++
@@ -1,6 +0,0 @@
-create or replace database bild_demo;
-use database bild_demo;
-create or replace schema IMDB_DATA;
-use schema imdb_data;
-
-create or re
